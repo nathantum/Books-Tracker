9ca698f26bbefaa190a789644f1a818220feafb1
Nathan Tum <nathantum@Nathans-MacBook-Air.local>
2023-01-17 20:26:01 -0600

    updated the entire project

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
 app.use('/api/books', booksRoutes)
 app.use('/api/user', userRoutes)
 
-
+const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
